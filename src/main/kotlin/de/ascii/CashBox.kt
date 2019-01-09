f478feb0fcba331439919e0845ca9bc25f996d7c
Lars Westermann <lars-westermann@live.de>
2019-01-09 18:04:33 +0100

    Add back button mode

--- src/main/kotlin/de/ascii/CashBox.kt
+++ src/main/kotlin/de/ascii/CashBox.kt
@@ -3,6 +3,7 @@ package de.ascii
 import de.ascii.coin.CoinBox
 import de.ascii.note.NoteBox
 import de.westermann.kobserve.basic.mapBinding
+import de.westermann.kobserve.basic.property
 import de.westermann.kwebview.View
 import de.westermann.kwebview.ViewCollection
 import de.westermann.kwebview.components.boxView
@@ -13,15 +14,27 @@ import de.westermann.kwebview.format
 class CashBox(
         private val cash: Cash
 ) : ViewCollection<View>(createHtmlView()) {
+
+    val calculateModeProperty = property(false)
+    var calculateMode by calculateModeProperty
+
+    private var backCash = CashEntry()
+
     init {
         boxView {
             classList += "header-box"
 
             boxView {
-                textView("Reset") {
+                textView(calculateModeProperty.mapBinding { if (it) "Back" else "Reset" }) {
                     onClick {
-                        cash.shift(CashEntry(), true)
-                        cash.previousCash = CashEntry()
+                        if (calculateMode) {
+                            cash.shift(backCash, false)
+                            cash.previousCash = CashEntry()
+                            calculateMode = false
+                        } else {
+                            cash.shift(CashEntry(), true)
+                            cash.previousCash = CashEntry()
+                        }
                     }
                 }
             }
@@ -29,7 +42,11 @@ class CashBox(
             textView(cash.totalProperty.mapBinding { "${it.format(2)} €" })
 
             boxView {
-                textView("Calculate")
+                textView("Calculate") {
+                    onClick {
+                        calculateMode = true
+                    }
+                }
             }
         }
         +CoinBox(cash)
